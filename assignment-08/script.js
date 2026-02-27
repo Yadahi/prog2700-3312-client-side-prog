f26c790da9280bb8d93fb93a08ed0e8ab544b358
Viliam Vateha <vatvil@gmail.com>
2026-02-27 16:22:12 -0400

    style message app

---
+++ assignment-08/script.js
@@ -0,0 +1,11 @@
+/* 
+Viliam Vateha
+Assignment 8
+*/
+
+//Type a message
+//Display the message inside a card
+//Change the card color
+//Add a small note dynamically
+//Remove the note
+//Reset the card
